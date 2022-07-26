2fe0b78c702fc5352d22704dcc81e02309dcd679
Oleg Vdovenko <oleg.olhapi@gmail.com>
2022-07-26 18:08:49 +0300

    [FEAT] try parcel

---
+++ dist/types.d.ts
@@ -0,0 +1,9 @@
+export interface Response<T = void> {
+    status: 'error' | 'success';
+    message: string;
+    id?: string;
+    statusCode?: number;
+    data?: T;
+}
+
+//# sourceMappingURL=types.d.ts.map
